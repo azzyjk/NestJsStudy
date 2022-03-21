7fd9eedf9def6376606284ea6b4c267425c4b56c
azzyjk <56161518+azzyjk@users.noreply.github.com>
2022-03-21 18:47:15 +0900

    feat(service): User 반환하는 함수 생성

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,19 +1,22 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserInput } from './dto/create-user.input';
 import { UpdateUserInput } from './dto/update-user.input';
+import { User } from './entities/user.entity';
 
 @Injectable()
 export class UserService {
+  userList = [new User(1, 23), new User(2, 25)];
+
   create(createUserInput: CreateUserInput) {
     return 'This action adds a new user';
   }
 
-  findAll() {
-    return `This action returns all user`;
+  findAll(): User[] {
+    return this.userList;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  findOne(id: number): User {
+    return this.userList[id];
   }
 
   update(id: number, updateUserInput: UpdateUserInput) {
